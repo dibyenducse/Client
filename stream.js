eceec27ba581eace3d48d365ba3fcb8578614290
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-13 20:13:35 +0600

    stream data

--- stream.js
+++ stream.js
@@ -1,7 +1,21 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const ourReadStream = fs.createReadStream(`${__dirname}/myfile.txt`);
+// const ourReadStream = fs.createReadStream(`${__dirname}/myfile.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+// ourReadStream.on('data', (chunk) => {
+//     console.log(chunk.toString());
+// });
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is resolved');
+    }, 3000);
 });
+
+async function print() {
+    console.log('Hi');
+    let hel = await promise;
+    console.log('hel');
+}
+
+print();
