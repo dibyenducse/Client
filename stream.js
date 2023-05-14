42c9c66813a1bd1fbcc4564833b3eb8166702d3c
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-14 14:44:50 +0600

    server created

--- stream.js
+++ stream.js
@@ -5,17 +5,3 @@
 // ourReadStream.on('data', (chunk) => {
 //     console.log(chunk.toString());
 // });
-
-let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is resolved');
-    }, 3000);
-});
-
-async function print() {
-    console.log('Hi');
-    let hel = await promise;
-    console.log('hel');
-}
-
-print();
